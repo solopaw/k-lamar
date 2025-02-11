172ef1d5c106db818bbea729a24de29a2dcd3a8a
solopaw <adamjoelfraser@icloud.com>
2025-02-11 16:03:56 +1000

    Update search and add table and select components

--- lib/search/search.ts
+++ lib/search/search.ts
@@ -2,12 +2,12 @@ import * as Commerce from "commerce-kit";
 import { unstable_cache } from "next/cache";
 import { simpleSearch } from "./simplesearch";
 
-type CB = (...query:string[]|any[])=>(Promise<Commerce.MappedProduct[]|void|undefined>)
+type CB = (query: string, ...args: (string | any)[]) => Promise<Commerce.MappedProduct[] | void | undefined>;
 export const searchProducts = unstable_cache<CB>(
-	async (query: string,...args:string[]|any[]) => {
+	async (query: string) => {
 		const products = await Commerce.productBrowse({ first: 100 });
 		const searchResults = simpleSearch(products, query);
-		return searchResults.map((sr) => products.find((p) => p.id === sr.id)).filter(Boolean);
+		return searchResults.map((sr) => products.find((p) => p.id === sr.id)!).filter(Boolean) as Commerce.MappedProduct[];
 	},
 	["search", "products"],
 	{
